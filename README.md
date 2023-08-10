3ca1f562117059110781d17d46c50bf1097e83c6
harleigh <harleigh.marsh@gmail.com>
2023-08-10 14:45:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simply learning React; a little calculator app from youtuber Tyler Potts, who can be found at
+
+https://www.youtube.com/@TylerPotts
---
+++ src/basicCalc.js
@@ -0,0 +1,20 @@
+//every begining has a begining, and this is this begining. And, being that
+//this is the begining, take it with a grain of salt as there is quite a road
+//yet being paved; as I am learning, all code and documentation are "as is"
+
+
+//import { useState } from 'react';
+
+//"export default"  makes this the main component in the file.
+//returns a component that contains the filter table with a little
+//header above
+export default function SimpleCalculator() {
+
+    return (
+        <>
+        <h1>Hello World!</h1>
+        </>
+    );
+
+}// end main function
+
